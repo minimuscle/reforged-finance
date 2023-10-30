cf3935be54c7d9846cd3f45ca8553aa069e5d40d
minimuscle <joshthiele@live.com.au>
2023-10-30 22:00:13 +1100

    added budget page

---
+++ app/budget/page.tsx
@@ -0,0 +1,10 @@
+import { Text, Title } from "@mantine/core"
+
+export default function Budget() {
+  return (
+    <div>
+      <Title>Budget</Title>
+      <Text>Personal Finance Budget</Text>
+    </div>
+  )
+}
