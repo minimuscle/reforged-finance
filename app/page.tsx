d379d835b9d5a124835b0053745de162142d1167
Josh Thiele <j.thiele@duttongroup.com.au>
2023-10-31 13:29:55 +1100

    added tanstack table, charts and charts.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,62 @@
-import { Text, Title } from "@mantine/core"
+"use client"
+import { Text, Title as Heading, Flex } from "@mantine/core"
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js"
+import { Line } from "react-chartjs-2"
 
-export const metadata = {
-  title: "Personal Finance",
-  description: "A Secure Personal Finance Application",
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "right" as const,
+    },
+    title: {
+      display: true,
+      text: "Cash Chart",
+    },
+  },
+}
+
+const labels = ["January", "February", "March", "April", "May"]
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: "Cash",
+      data: [9573, 2432, 3425, 5443, 2343],
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+  ],
 }
 
 export default function HomePage() {
   return (
     <div>
-      <Title>Dashboard</Title>
+      <Heading>Dashboard</Heading>
       <Text>Personal Finance Dashboard</Text>
+      <Flex w="50%">
+        <Line options={options} data={data} />
+      </Flex>
     </div>
   )
 }
