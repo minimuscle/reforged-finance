62d088dfbea54d4cdd28c09d84b5ecc1cf8c821d
minimuscle <joshthiele@live.com.au>
2023-11-13 19:01:17 +1100

    basic premium styling

--- app/routes/_app.premium.jsx
+++ app/routes/_app.premium.jsx
@@ -1,6 +1,20 @@
 import React from "react"
-import { Center, Paper, Space, Stack, Table, Text, Title } from "@mantine/core"
+import {
+  Box,
+  Button,
+  Center,
+  Divider,
+  Flex,
+  Paper,
+  SimpleGrid,
+  Space,
+  Stack,
+  Table,
+  Text,
+  Title,
+} from "@mantine/core"
 import "../styles/styles.css"
+import { RiBardFill, RiBardLine } from "react-icons/ri/index.js"
 
 export const meta = () => {
   return [{ title: "Premium | Personal Finance" }]
@@ -36,10 +50,41 @@ const data = [
 export default function Premium() {
   return (
     <>
-      <Stack align="center">
+      <Stack align="center" mb="50px">
         <Title>Premium</Title>
         <Text>Get Premium</Text>
       </Stack>
+      <Flex justify="center" gap="xl" wrap="wrap">
+        <Paper withBorder shadow="xl" p="xl" w="300px">
+          <RiBardLine size="1.5em" />
+          <Title>Base</Title>
+          <Text>Free plan for starters</Text>
+          <Text>
+            $<Text span>0</Text>
+          </Text>
+          <Divider m="30px 0" />
+          <Text>History</Text>
+          <Button mt="30px" disabled fullWidth>
+            Free
+          </Button>
+        </Paper>
+        <Paper withBorder shadow="xl" p="xl" w="300px">
+          <Title align="center">Monthly</Title>
+          <Divider m="30px 0" />
+          <Text>History</Text>
+          <Button mt="30px" color="violet" fullWidth>
+            $5 / month (AUD)
+          </Button>
+        </Paper>
+        <Paper withBorder shadow="xl" p="xl" w="300px">
+          <Title align="center">Lifetime</Title>
+          <Divider m="30px 0" />
+          <Text>History</Text>
+          <Button mt="30px" color="violet" fullWidth>
+            $60
+          </Button>
+        </Paper>
+      </Flex>
     </>
   )
 }
