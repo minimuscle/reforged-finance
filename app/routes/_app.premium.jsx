10fa7b9224f6d3d48ec729ba1bc8e6d78728d932
minimuscle <joshthiele@live.com.au>
2023-11-13 21:41:57 +1100

    updated premium screen and favicon

--- app/routes/_app.premium.jsx
+++ app/routes/_app.premium.jsx
@@ -1,10 +1,13 @@
-import React from "react"
+import React, { useState } from "react"
 import {
+  Badge,
   Box,
   Button,
   Center,
   Divider,
   Flex,
+  Group,
+  List,
   Paper,
   SimpleGrid,
   Space,
@@ -14,7 +17,11 @@ import {
   Title,
 } from "@mantine/core"
 import "../styles/styles.css"
-import { RiBardFill, RiBardLine } from "react-icons/ri/index.js"
+import {
+  RiVipCrownLine,
+  RiLifebuoyLine,
+  RiBardLine,
+} from "react-icons/ri/index.js"
 
 export const meta = () => {
   return [{ title: "Premium | Personal Finance" }]
@@ -48,6 +55,7 @@ const data = [
 ]
 
 export default function Premium() {
+  const [selected, setSelected] = useState("none")
   return (
     <>
       <Stack align="center" mb="50px">
@@ -59,29 +67,110 @@ export default function Premium() {
           <RiBardLine size="1.5em" />
           <Title>Base</Title>
           <Text>Free plan for starters</Text>
-          <Text>
-            $<Text span>0</Text>
+          <Text className="pricing">
+            $
+            <Text className="large" span>
+              0
+            </Text>
+            <Text className="small" span>
+              / free
+            </Text>
           </Text>
           <Divider m="30px 0" />
-          <Text>History</Text>
+          <List>
+            <List.Item>Net Worth Overview</List.Item>
+            <List.Item>Historical Data</List.Item>
+            <List.Item>Basic Budget</List.Item>
+            <List.Item>Liabilities / Debts Tracking</List.Item>
+            <List.Item>Super Tracking</List.Item>
+            <List.Item>Stocks</List.Item>
+          </List>
           <Button mt="30px" disabled fullWidth>
-            Free
+            Current Plan
           </Button>
         </Paper>
-        <Paper withBorder shadow="xl" p="xl" w="300px">
-          <Title align="center">Monthly</Title>
+        <Paper
+          withBorder
+          bg={selected === "premium" ? "violet" : ""}
+          className={selected === "premium" ? "white" : ""}
+          shadow="xl"
+          p="xl"
+          w="300px"
+        >
+          <Group justify="space-between" mb="5px">
+            <RiVipCrownLine size="1.5em" />
+            <Badge
+              variant="gradient"
+              gradient={{ from: "blue", to: "teal", deg: 90 }}
+            >
+              Most Popular
+            </Badge>
+          </Group>
+          <Title>Premium</Title>
+          <Text>A small fee, all the content</Text>
+          <Text className="pricing">
+            $
+            <Text className="large" span>
+              5
+            </Text>
+            <Text className="small" span>
+              / month
+            </Text>
+          </Text>
           <Divider m="30px 0" />
-          <Text>History</Text>
-          <Button mt="30px" color="violet" fullWidth>
-            $5 / month (AUD)
+          <List>
+            <List.Item>Detailed Budgeting System</List.Item>
+            <List.Item>Export Overview as PDF</List.Item>
+            <List.Item>Export History Details</List.Item>
+            <List.Item>Live Stock Tracking</List.Item>
+            <List.Item>FIRE Dashboard</List.Item>
+            <List.Item>Premium Support</List.Item>
+          </List>
+          <Button
+            mt="30px"
+            color="violet"
+            variant={selected === "premium" ? "white" : "filled"}
+            fullWidth
+            onClick={() => setSelected("premium")}
+          >
+            Select{selected === "premium" ? "ed" : ""}
           </Button>
         </Paper>
-        <Paper withBorder shadow="xl" p="xl" w="300px">
-          <Title align="center">Lifetime</Title>
+        <Paper
+          withBorder
+          bg={selected === "lifetime" ? "violet" : ""}
+          className={selected === "lifetime" ? "white" : ""}
+          shadow="xl"
+          p="xl"
+          w="300px"
+        >
+          <RiLifebuoyLine size="1.5em" />
+          <Title>Lifetime</Title>
+          <Text>Premium Forever.</Text>
+          <Text className="pricing">
+            $
+            <Text className="large" span>
+              60
+            </Text>
+            <Text className="small" span>
+              / lifetime
+            </Text>
+          </Text>
           <Divider m="30px 0" />
-          <Text>History</Text>
-          <Button mt="30px" color="violet" fullWidth>
-            $60
+          <List>
+            <List.Item>All Premium Features</List.Item>
+            <List.Item>One-Time Fee</List.Item>
+            <List.Item>No On-Going Costs</List.Item>
+            <List.Item>Same Premium Support</List.Item>
+          </List>
+          <Button
+            mt="80px"
+            color="violet"
+            variant={selected === "lifetime" ? "white" : "filled"}
+            fullWidth
+            onClick={() => setSelected("lifetime")}
+          >
+            Select{selected === "lifetime" ? "ed" : ""}
           </Button>
         </Paper>
       </Flex>
