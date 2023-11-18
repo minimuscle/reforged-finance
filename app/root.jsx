8a9d423cab405f71c1abbe658460f8e6be827596
minimuscle <joshthiele@live.com.au>
2023-11-18 20:41:53 +1100

    refactored to remove _app

--- app/root.jsx
+++ app/root.jsx
@@ -7,13 +7,31 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useRevalidator,
+  useActionData,
 } from "@remix-run/react"
 import "@mantine/core/styles.css"
-import { ColorSchemeScript } from "@mantine/core"
+import {
+  AppShell,
+  Avatar,
+  Burger,
+  Button,
+  ColorSchemeScript,
+  Group,
+  MantineProvider,
+  Text,
+} from "@mantine/core"
 import "./global.css"
 import { SupabaseContext } from "./contexts/SupabaseContext"
 import { createBrowserClient } from "@supabase/ssr"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { DatabaseContext } from "./contexts/DatabaseContext"
+import { PremiumMemberContext } from "./contexts/PremiumMemberContext"
+import LoadDatabaseModal from "./components/LoadDatabaseModal/LoadDatabaseModal"
+import LogoButton from "./components/LogoButton/LogoButton"
+import Sidebar from "./components/Sidebar/Sidebar"
+import { useDisclosure } from "@mantine/hooks"
+import theme from "~/util/theme"
 
 export const links = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
@@ -38,6 +56,53 @@ export default function App() {
   const [supabase] = useState(() =>
     createBrowserClient(env.DATABASE_URL, env.DB_KEY)
   )
+  const [opened, { toggle }] = useDisclosure(false)
+  const [loginOpen, { toggle: loginToggle }] = useDisclosure(false)
+  const [signUpOpen, { toggle: signUpToggle }] = useDisclosure(false)
+  const { user } = useLoaderData()
+  const res = useActionData()
+  const [database, setDatabase] = useState(user?.user?.id ? true : false)
+  const [modalOpen, { toggle: modalToggle }] = useDisclosure(!database)
+  const revalidator = useRevalidator()
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange(() => {
+      revalidator.revalidate()
+      if (user?.user?.id) {
+        setDatabase(true)
+      } else {
+        setDatabase(false)
+      }
+    })
+
+    console.log("database: ", database)
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [supabase, database, user?.user?.id])
+
+  const signUp = () => {
+    supabase.auth.signUp({
+      email: "joshthiele@live.com.au",
+      password: "password",
+    })
+  }
+
+  const login = () => {
+    loginToggle()
+    modalToggle()
+    /*supabase.auth.signInWithPassword({
+      email: "joshthiele@live.com.au",
+      password: "password",
+    })*/
+  }
+
+  const logout = () => {
+    supabase.auth.signOut()
+  }
 
   return (
     <html lang="en">
@@ -50,7 +115,52 @@ export default function App() {
       </head>
       <body>
         <SupabaseContext.Provider value={supabase}>
-          <Outlet />
+          <DatabaseContext.Provider value={database}>
+            <PremiumMemberContext.Provider>
+              <MantineProvider theme={theme}>
+                <LoadDatabaseModal
+                  opened={modalOpen}
+                  close={modalToggle}
+                  login={login}
+                />
+                <AppShell
+                  header={{ height: 60 }}
+                  navbar={{
+                    width: 300,
+                    breakpoint: "sm",
+                    collapsed: { mobile: !opened },
+                  }}
+                  padding="md"
+                >
+                  <AppShell.Header>
+                    <Group h="100%" px="md" align="center">
+                      <Burger
+                        opened={opened}
+                        onClick={toggle}
+                        hiddenFrom="sm"
+                        size="sm"
+                      />
+                      <LogoButton />
+
+                      <Group pos="absolute" right={10}>
+                        <Text align="right">{user?.user?.email}</Text>
+                        <Avatar variant="outline" radius="xl" src="" />
+                        <Button onClick={() => signUp()}>Sign Up</Button>
+                        <Button onClick={() => login()}>Login</Button>
+                        <Button onClick={() => logout()}>Logout</Button>
+                      </Group>
+                    </Group>
+                  </AppShell.Header>
+                  <AppShell.Navbar p="md">
+                    <Sidebar />
+                  </AppShell.Navbar>
+                  <AppShell.Main>
+                    <Outlet />
+                  </AppShell.Main>
+                </AppShell>
+              </MantineProvider>
+            </PremiumMemberContext.Provider>
+          </DatabaseContext.Provider>
         </SupabaseContext.Provider>
         <ScrollRestoration />
         <LiveReload />
