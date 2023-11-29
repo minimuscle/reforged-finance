50fe29852b22e1a1c2b018b07db994679f3c3d99
minimuscle <joshthiele@live.com.au>
2023-11-29 23:28:25 +1100

    finished cash savings

--- app/components/Widgets/Cash/CashSavings/CashSavings.jsx
+++ app/components/Widgets/Cash/CashSavings/CashSavings.jsx
@@ -22,12 +22,61 @@ const CashSavings = () => {
 
   //sort in case the month was added at a different time, ID might not be in order
   const thisYear = history
-    .filter((item) => item.year == year)
-    .sort((a, b) => a.month - b.month)
+    .filter(
+      (item) => item.year == year || (item.month == 12 && item.year == year - 1)
+    )
+    .sort((a, b) => {
+      // Custom sorting logic
+      if (a.year == year - 1 && a.month == 12) {
+        return -1 // Move items with month "12" and year "year - 1" to the front
+      } else if (b.year == year - 1 && b.month == 12) {
+        return 1 // Move items with month "12" and year "year - 1" to the front
+      } else {
+        // Normal sorting based on month
+        return a.month - b.month
+      }
+    })
+
+  const sortedHistory = history
+    .sort((a, b) => {
+      // Compare by 'year'
+      if (a.year !== b.year) {
+        return a.year - b.year
+      }
+      // If 'year' is the same, compare by 'month'
+      return a.month - b.month
+    })
+    .reverse()
+
+  console.log(sortedHistory)
+
+  const cashComparisons = []
+
+  // Iterate over the array for 12 months
+  for (let i = 0; i < 13; i++) {
+    const currentMonthCash = sortedHistory[i].cash
+    const previousMonthCash = sortedHistory[i + 1].cash
+    // Compare the 'cash' values and push the result into the new array
+    cashComparisons.push(currentMonthCash - previousMonthCash)
+  }
+  console.log("compare:", cashComparisons)
+  const averageComparisonResult = cashComparisons.reduce(
+    (acc, num) => acc + num,
+    0
+  )
+  console.log("compareRes:", averageComparisonResult / cashComparisons.length)
+  console.log(cashComparisons.length)
+
+  const averageMonthlyDifference =
+    averageComparisonResult / cashComparisons.length
 
   const monthlyCashSavings = thisMonth.cash - lastMonth.cash
   const yearlyCashSavings =
     thisYear[thisYear.length - 1].cash - thisYear[0].cash
+
+  const predictedSavings =
+    yearlyCashSavings + averageMonthlyDifference * (12 - month)
+  const predictedCash = thisMonth.cash + predictedSavings
 
   //Get today's month
   const monthLong = Intl.DateTimeFormat("en-US", {
@@ -39,7 +88,7 @@ const CashSavings = () => {
         <Title>{monthLong} Cash Savings</Title>
         <Table verticalSpacing="md" withTableBorder striped>
           <Table.Tbody>
-            <Table.Tr c={monthlyCashSavings > 0 ? "green" : "red"}>
+            <Table.Tr>
               <Table.Td>
                 <Text>Cash Savings This Month</Text>
               </Table.Td>
@@ -54,7 +103,7 @@ const CashSavings = () => {
                 </Badge>
               </Table.Td>
             </Table.Tr>
-            <Table.Tr c={yearlyCashSavings > 0 ? "green" : "red"}>
+            <Table.Tr>
               <Table.Td>
                 <Text>Cash Savings This Year</Text>
               </Table.Td>
@@ -69,35 +118,49 @@ const CashSavings = () => {
                 </Badge>
               </Table.Td>
             </Table.Tr>
-            <Table.Tr c={yearlyCashSavings > 0 ? "green" : "red"}>
+            <Table.Tr>
               <Table.Td>
                 <Text>Average Cash Savings per Month</Text>
               </Table.Td>
               <Table.Td align="right">
                 <Badge
-                  color={yearlyCashSavings > 0 ? "green" : "red"}
+                  color={averageMonthlyDifference > 0 ? "green" : "red"}
                   variant="light"
                   size="xl"
                   radius="sm"
                 >
-                  {moneyFormatter.format(yearlyCashSavings)}
+                  {moneyFormatter.format(averageMonthlyDifference)}
                 </Badge>
               </Table.Td>
             </Table.Tr>
             <Table.Tr>
               <Table.Td>
-                <Text fw={700}>Average Cash Savings per Month</Text>
+                <Text>Predicted Cash Savings EOY</Text>
               </Table.Td>
-              <Table.Td>
-                <Text align="right">{moneyFormatter.format(1000)}</Text>
+              <Table.Td align="right">
+                <Badge
+                  color={predictedSavings > 0 ? "green" : "red"}
+                  variant="light"
+                  size="xl"
+                  radius="sm"
+                >
+                  {moneyFormatter.format(predictedSavings)}
+                </Badge>
               </Table.Td>
             </Table.Tr>
             <Table.Tr>
               <Table.Td>
-                <Text fw={700}>Predicted Cash EOY</Text>
+                <Text>Predicted Total Cash EOY</Text>
               </Table.Td>
-              <Table.Td>
-                <Text align="right">{moneyFormatter.format(1000)}</Text>
+              <Table.Td align="right">
+                <Badge
+                  color={predictedCash > 0 ? "green" : "red"}
+                  variant="light"
+                  size="xl"
+                  radius="sm"
+                >
+                  {moneyFormatter.format(predictedCash)}
+                </Badge>
               </Table.Td>
             </Table.Tr>
           </Table.Tbody>
