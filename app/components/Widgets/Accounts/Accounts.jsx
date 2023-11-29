bc30242def06b3dc717ca847611544ec9e5fb491
minimuscle <joshthiele@live.com.au>
2023-11-29 14:22:53 +1100

    add account feature

--- app/components/Widgets/Accounts/Accounts.jsx
+++ app/components/Widgets/Accounts/Accounts.jsx
@@ -10,7 +10,18 @@ import {
   arrayMove,
   verticalListSortingStrategy,
 } from "@dnd-kit/sortable"
-import { Box, Button, Grid, Group, Paper, Text, Title } from "@mantine/core"
+import {
+  ActionIcon,
+  Box,
+  Button,
+  Flex,
+  Grid,
+  Group,
+  Input,
+  Paper,
+  Text,
+  Title,
+} from "@mantine/core"
 import { useFetcher, useLoaderData } from "@remix-run/react"
 import SortableAccount from "./SortableAccount"
 import { useEffect, useState } from "react"
@@ -19,14 +30,17 @@ import {
   restrictToVerticalAxis,
   restrictToWindowEdges,
 } from "@dnd-kit/modifiers"
+import { RiCheckFill } from "react-icons/ri/index.js"
 
 const Accounts = () => {
   const { cash } = useLoaderData()
   const [accounts, setAccounts] = useState(cash)
+  const [editing, setEditing] = useState(false)
   const fetcher = useFetcher()
 
   useEffect(() => {
     setAccounts(cash)
+    setEditing(false)
   }, [cash])
 
   const handleDragEnd = (event) => {
@@ -84,6 +98,47 @@ const Accounts = () => {
             ))}
           </SortableContext>
         </DndContext>
+        {editing ? (
+          <Paper className='account' shadow='xs' withBorder p='sm' mb='10px'>
+            <fetcher.Form method='POST'>
+              <Grid>
+                <Grid.Col span={5.5} align='left'>
+                  <Input name='bank_name' placeholder='Bank' />
+                </Grid.Col>
+                <Grid.Col span={3} align='left'>
+                  <Input name='balance' type='number' placeholder='0' />
+                </Grid.Col>
+                <Grid.Col span={2} align='left'>
+                  <input type='hidden' name='currency' value='AUD' />
+                  <Text c='lightGray'>AUD</Text>
+                </Grid.Col>
+                <Grid.Col span={1.5} align={"center"}>
+                  <ActionIcon
+                    color='green'
+                    variant='light'
+                    m={"0 -5px"}
+                    type='submit'
+                    name='_action'
+                    value='addBank'
+                  >
+                    <RiCheckFill className='always' />
+                  </ActionIcon>
+                </Grid.Col>
+              </Grid>
+            </fetcher.Form>
+          </Paper>
+        ) : (
+          <Flex>
+            <Button
+              size='xs'
+              color='gray'
+              variant='light'
+              onClick={() => setEditing(true)}
+            >
+              New Account
+            </Button>
+          </Flex>
+        )}
       </Box>
     </Paper>
   )
