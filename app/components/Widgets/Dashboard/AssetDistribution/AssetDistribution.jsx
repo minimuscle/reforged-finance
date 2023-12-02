1655af997d7168489f0080dbcbd33a4e2e96913f
minimuscle <joshthiele@live.com.au>
2023-12-02 16:30:28 +1100

    added asset distribution

--- app/components/Widgets/Dashboard/AssetDistribution/AssetDistribution.jsx
+++ app/components/Widgets/Dashboard/AssetDistribution/AssetDistribution.jsx
@@ -1,32 +1,29 @@
-import { Badge, Stack, Text, Title } from "@mantine/core"
+import { Badge, Paper, Stack, Text, Title } from "@mantine/core"
 import NetworthChart from "../../../Charts/NetworthChart"
 import { useLoaderData } from "@remix-run/react"
 import { ClientOnly } from "remix-utils/client-only"
-import { formatter } from "../../../../util"
+import { formatter, getThisMonthData } from "../../../../util"
 
 const AssetDistribution = () => {
-  const { data } = useLoaderData()
-  console.log(data)
+  const { history } = useLoaderData()
+  const latest = getThisMonthData(history)
 
   const getNetWorth = () => {
-    if (data.length <= 0) return 0
-    const latest = data[data.length - 1]
-    const netWorth = latest?.cash + latest?.super + latest?.debts
-
+    const netWorth = latest.cash + latest.super
     return formatter.format(netWorth)
   }
 
   return (
-    <>
+    <Paper shadow="xl" p="md" withBorder h="100%" miw="500px">
       <Stack align="center" gap="1">
         <Title>Asset Distribution</Title>
-        <Text>Total Net Worth</Text>
+        <Text>Total Assets</Text>
         <Badge p={"20"} size="xl" radius="sm" variant="light">
           <Title order={2}>{getNetWorth()}</Title>
         </Badge>
       </Stack>
-      <ClientOnly>{() => <NetworthChart />}</ClientOnly>
-    </>
+      <ClientOnly>{() => <NetworthChart data={latest} />}</ClientOnly>
+    </Paper>
   )
 }
 
--- app/util/util.ts
+++ app/util/util.ts
@@ -11,16 +11,20 @@
 
 /**This gets the month based on the date, and the format which defaults to 2-digit
  * Its just a shorter version of the Intl.DateTimeFormat
+ * 
+ * Provide no parameters to get the current month
  */
-export const getMonth = (date: Date, format: Intl.DateTimeFormatOptions['month'] = "2-digit"): number | string => {
+export const getMonth = (date: number = Date.now(), format: Intl.DateTimeFormatOptions['month'] = "2-digit") => {
   return Intl.DateTimeFormat('en-AU', { month: format }).format(date)
 }
 
 /**This gets the year based on the date, and the format which defaults to numeric (full)
  * Its just a shorter version of the Intl.DateTimeFormat
+ * 
+ * Provide no parameters to get the current year
  */
-export const getYear = (date: Date, format: Intl.DateTimeFormatOptions['year'] = "numeric"): number => {
-  const year = Intl.DateTimeFormat('en-AU', { year: format }).format(date)
+export const getYear = (date: number = Date.now(), format: Intl.DateTimeFormatOptions['year'] = "numeric") => {
+  return Intl.DateTimeFormat('en-AU', { year: format }).format(date)
 }
 
 /**This gets the last month from an array of data, it requires a month in 2-digit format, and a full year
@@ -31,8 +35,16 @@ type LastMonthData = {
   year: string,
   data: object
 }
-export const getLastMonthData = (data:any, month: number, year: number) => {
-  console.log(data)
+export const getLastMonthData = (data: any, month: string = getMonth(), year: string = getYear()) => {
+  return data.find((item: any) => { 
+    return item.month == (parseInt(month) - 1) && item.year == year
+  })
+}
+
+export const getThisMonthData = (data: any, month: string = getMonth(), year: string = getYear()) => {
+  return data.find(
+    (item: any) => item.month == month && item.year == year
+  )
 }
 
 /**Money formatter gets money in the right format for the currency
