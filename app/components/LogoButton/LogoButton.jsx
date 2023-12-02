9ab3fe14c15618caa6a4849f2e512ba64e5e5e1c
minimuscle <joshthiele@live.com.au>
2023-12-03 01:25:22 +1100

    added alpha tag

--- app/components/LogoButton/LogoButton.jsx
+++ app/components/LogoButton/LogoButton.jsx
@@ -10,7 +10,7 @@ function LogoButton() {
         <Group>
           <Image w="40px" src={Logo} />
           <Title size="h2" className="logoTitle">
-            WealthForge
+            WealthForge (Alpha)
           </Title>
         </Group>
       </Button>
