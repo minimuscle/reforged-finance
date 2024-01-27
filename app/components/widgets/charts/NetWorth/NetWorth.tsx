1f702e91f30ceecfcb53bfcace5ff9041cdcaa39
Josh Thiele <j.thiele@duttongroup.com.au>
2024-01-28 00:11:10 +1100

    css adjustment

--- app/components/widgets/charts/NetWorth/NetWorth.tsx
+++ app/components/widgets/charts/NetWorth/NetWorth.tsx
@@ -1,34 +1,6 @@
 import { LineChart } from "@tremor/react"
 import useHistory from "~/utils/hooks/useHistory"
 
-const chartdata = [
-  {
-    year: 1970,
-    "Export Growth Rate": 2.04,
-    "Import Growth Rate": 1.53,
-  },
-  {
-    year: 1971,
-    "Export Growth Rate": 1.96,
-    "Import Growth Rate": 1.58,
-  },
-  {
-    year: 1972,
-    "Export Growth Rate": 1.96,
-    "Import Growth Rate": 1.61,
-  },
-  {
-    year: 1973,
-    "Export Growth Rate": 1.93,
-    "Import Growth Rate": 1.61,
-  },
-  {
-    year: 1974,
-    "Export Growth Rate": 1.88,
-    "Import Growth Rate": 1.67,
-  },
-]
-
 const valueFormatter = (number: number) => {
   if (number < 0) {
     return `-$${new Intl.NumberFormat("us").format(number * -1).toString()}`
