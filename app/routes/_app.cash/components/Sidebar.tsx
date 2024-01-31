bb450207b744d1adda109507113a8a97073b4736
Josh Thiele <j.thiele@duttongroup.com.au>
2024-01-31 22:05:48 +1100

    added new account button

--- app/routes/_app.cash/components/Sidebar.tsx
+++ app/routes/_app.cash/components/Sidebar.tsx
@@ -1,14 +1,30 @@
-import { Title } from "@mantine/core"
+import { Button, Title } from "@mantine/core"
 import BankAccounts from "./BankAccounts"
 import styles from "../cash.module.css"
+import { useFetcher } from "@remix-run/react"
+import { FaPlus } from "react-icons/fa/index.js"
 
 const Sidebar = () => {
+  const fetcher = useFetcher()
   return (
     <>
       <Title className={styles.header} size={"h2"}>
         Bank Accounts
       </Title>
       <BankAccounts />
+      <fetcher.Form method="POST">
+        <Button
+          type="submit"
+          name="intent"
+          value="createCash"
+          size="xs"
+          variant="subtle"
+          color="gray"
+          leftSection={<FaPlus />}
+        >
+          Add Account
+        </Button>
+      </fetcher.Form>
     </>
   )
 }
