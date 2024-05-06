de02bf445a43962c6757837c682673b4e914b34b
Josh Thiele <j.thiele@duttongroup.com.au>
2024-05-06 16:35:02 +1000

    added mobile navigation

--- app/routes/_app+/_layout/Header.tsx
+++ app/routes/_app+/_layout/Header.tsx
@@ -1,18 +1,29 @@
-import { ActionIcon, Title } from '@mantine/core'
+import { ActionIcon, Drawer, Title } from '@mantine/core'
 import classes from './_layout.module.css'
 import { MdMenu } from 'react-icons/md'
 import { BiListOl } from 'react-icons/bi'
 import { BsList, BsPlus } from 'react-icons/bs'
+import { useDisclosure } from '@mantine/hooks'
+import { Link, NavLink } from '@remix-run/react'
 
 export default function Header() {
+  const [opened, { open, close }] = useDisclosure()
+
   return (
     <div className={classes.header}>
-      <ActionIcon size={'lg'} variant='transparent' color='gray' aria-label='Settings'>
-        <BsList style={{ width: 24, height: 24 }} />
+      <Drawer opened={opened} onClose={close} title='Reforged Finance'>
+        <NavLink onClick={close} prefetch='viewport' to='/about'>
+          About
+        </NavLink>
+      </Drawer>
+      <ActionIcon onClick={open} size={'xl'} variant='transparent' color='gray' aria-label='Settings'>
+        <BsList style={{ width: 28, height: 28 }} />
       </ActionIcon>
-      <Title>Reforged Finance</Title>
-      <ActionIcon size={'lg'} variant='light' color='gray' aria-label='Settings'>
-        <BsPlus style={{ width: 24, height: 24 }} />
+      <Link to='/'>
+        <Title size={24}>Reforged Finance</Title>
+      </Link>
+      <ActionIcon size={'xl'} variant='light' color='gray' aria-label='Settings'>
+        <BsPlus style={{ width: 28, height: 28 }} />
       </ActionIcon>
     </div>
   )
