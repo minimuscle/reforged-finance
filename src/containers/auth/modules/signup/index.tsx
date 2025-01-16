9b3f3c53fbccdf88be8e848ccc695015a9994908
minimuscle <joshthiele@live.com.au>
2025-01-16 20:11:14 +1100

    feature: styled signup form

--- src/containers/auth/modules/signup/index.tsx
+++ src/containers/auth/modules/signup/index.tsx
@@ -1,7 +1,13 @@
-import { useForm } from "react-hook-form"
+import { FormProvider, useForm } from "react-hook-form"
 import "./_signup.css"
 import { Link } from "@tanstack/react-router"
 import { auth } from "../../queries"
+import { LogoCard } from "components/Logo"
+import { Card } from "components/Card"
+import { Text } from "components/Text"
+import { Flex } from "components/Flex"
+import { Input } from "components/Form/Input"
+import { Button, Space } from "@mantine/core"
 
 /******************************************************************
  *  COMPONENT START                                               *
@@ -18,25 +24,29 @@ export function Signup() {
 
   /*********  RENDER  *********/
   return (
-    <div className="signup">
-      <div>
-        <h1>Sign Up</h1>
-      </div>
-
-      <form onSubmit={methods.handleSubmit(handleSubmit)}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input {...methods.register("email")} />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input {...methods.register("password")} />
-        </div>
-        <p>
-          Already registered? <Link to="/login">Login here</Link>
-        </p>
-        <button type="submit">Signup</button>
-      </form>
+    <div className="login">
+      <LogoCard className="login__logo" />
+      <Card className="login__card">
+        <Text as="h1" size="xxl" alignCenter>
+          Sign Up
+        </Text>
+        <form onSubmit={methods.handleSubmit(handleSubmit)}>
+          <FormProvider {...methods}>
+            <Flex direction="column" gap="10px">
+              <Input.HookForm name="email" label="Email" />
+              <Input.HookForm name="password" label="Password" type="password" />
+              <Input.HookForm name="repeat_password" label="Repeat Password" type="password" />
+              <Space h={10} />
+              <Button color="sky" type="submit">
+                Signup
+              </Button>
+              <Text size="sm" color="gray">
+                Already registered? <Link to="/login">Login here</Link>
+              </Text>
+            </Flex>
+          </FormProvider>
+        </form>
+      </Card>
     </div>
   )
 }
