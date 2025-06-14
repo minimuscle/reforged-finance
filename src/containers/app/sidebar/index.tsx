df8d1b7958e59ac7578ff3086504797042a6f278
minimuscle <joshthiele@live.com.au>
2025-06-14 16:48:47 +1000

    feature: implemented posthog

--- src/containers/app/sidebar/index.tsx
+++ src/containers/app/sidebar/index.tsx
@@ -11,6 +11,7 @@ import { AnimatePresence, motion } from "motion/react"
 import { PremiumAd } from "containers/app/sidebar/components/premiumAd"
 import { useAppContext } from "containers/app/appContext"
 import { Link } from "@tanstack/react-router"
+import posthog from "posthog-js"
 
 /******************************************************************
  *  COMPONENT START                                               *
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
-import { ErrorComponent, RouterProvider, createRouter } from "@tanstack/react-router"
+import { RouterProvider, createRouter } from "@tanstack/react-router"
 import { routeTree } from "./routeTree.gen"
 import { QueryClientProvider } from "@tanstack/react-query"
 import { queryClient } from "./utils/query/queryClient"
 import { MantineProvider } from "@mantine/core"
 import { theme } from "./utils/theme"
 import { ErrorBoundary } from "components/Templates/ErrorBoundary"
+import { PostHogProvider } from "posthog-js/react"
 
 /******************************************************************
  *  SETUP                                                         *
@@ -36,11 +37,20 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <MantineProvider theme={theme}>
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-        </QueryClientProvider>
-      </MantineProvider>
+      <PostHogProvider
+        apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY!}
+        options={{
+          api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST!,
+          defaults: "2025-05-24",
+          person_profiles: "always",
+        }}
+      >
+        <MantineProvider theme={theme}>
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+          </QueryClientProvider>
+        </MantineProvider>
+      </PostHogProvider>
     </StrictMode>
   )
 }
