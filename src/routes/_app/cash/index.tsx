940fe4790df1c6b553d968123f26f8f5651e5ad3
minimuscle <joshthiele@live.com.au>
2026-02-18 20:58:08 +1100

    feat: cash page layout

--- src/routes/_app/cash/index.tsx
+++ src/routes/_app/cash/index.tsx
@@ -1,4 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Card } from 'components/Card'
+import { Flex } from 'components/Flex'
+import { Grid } from 'components/Grid'
 
 /******************************************************
  *   ROUTE START
@@ -12,5 +15,36 @@ export const Route = createFileRoute('/_app/cash/')({
  ******************************************************/
 function RouteComponent() {
   /***** RENDER *****/
-  return <div>Hello "/_app/cash/"!</div>
+  return (
+    <Grid columns="repeat(4, 1fr)" gap={20}>
+      <Grid.Col>
+        <Flex direction="column" gap={20}>
+          <Card>Savings Distribution</Card>
+          <Card>Cash Accounts</Card>
+        </Flex>
+      </Grid.Col>
+      <Grid.Col span={3}>
+        <Grid columns="repeat(3, 1fr)" gap={20}>
+          <Grid.Col>
+            <Card>Savings This Month</Card>
+          </Grid.Col>
+          <Grid.Col>
+            <Card>Average Savings Per Month</Card>
+          </Grid.Col>
+          <Grid.Col>
+            <Card>Predicted EOY Savings</Card>
+          </Grid.Col>
+          <Grid.Col span={3}>
+            <Card>Cash Total</Card>
+          </Grid.Col>
+          <Grid.Col span={3}>
+            <Card>Savings History</Card>
+          </Grid.Col>
+          <Grid.Col span={3}>
+            <Card>Savings Rate</Card>
+          </Grid.Col>
+        </Grid>
+      </Grid.Col>
+    </Grid>
+  )
 }
